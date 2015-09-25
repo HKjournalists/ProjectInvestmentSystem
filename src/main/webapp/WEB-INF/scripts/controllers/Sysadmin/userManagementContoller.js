5e560a9cdf6811912942fbcdf3d227e1deaf6175
柳栋明 <liudm@sdc.icbc.com.cn>
2015-09-25 22:09:10 +0800

    udpate table demo

---
+++ src/main/webapp/WEB-INF/scripts/controllers/Sysadmin/userManagementContoller.js
@@ -0,0 +1,38 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name sbAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sbAdminApp
+ */
+angular.module('sbAdminApp')
+.controller('paginationCtrl', ['$scope', function (scope) {
+    var
+        nameList = ['Pierre', 'Pol', 'Jacques', 'Robert', 'Elisa'],
+        familyName = ['Dupont', 'Germain', 'Delcourt', 'bjip', 'Menez'];
+
+    function createRandomItem() {
+        var
+            firstName = nameList[Math.floor(Math.random() * 4)],
+            lastName = familyName[Math.floor(Math.random() * 4)],
+            age = Math.floor(Math.random() * 100),
+            email = firstName + lastName + '@whatever.com',
+            balance = Math.random() * 3000;
+
+        return{
+            firstName: firstName,
+            lastName: lastName,
+            age: age,
+            email: email,
+            balance: balance
+        };
+    }
+
+		scope.itemsByPage=15;
+
+    scope.rowCollection = [];
+    for (var j = 0; j < 200; j++) {
+        scope.rowCollection.push(createRandomItem());
+    }
+}]);
